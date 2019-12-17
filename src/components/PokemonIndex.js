99196452e0ccba6fda3904449f35596af594d255
RubenV-Dev <r.will16@hotmail.com>
2019-12-17 11:17:40 -0500

    Done.

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,16 +5,82 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+
+  state={
+    pokeArray: [],
+    searchTerm: ""
+  }
+
+  handleSearch = (searchValue) => { 
+    this.setState({
+      searchTerm: searchValue
+    })
+  }
+
+  handleSubmit = (evt) => {
+    evt.preventDefault()
+    let pokeinfo = {
+      name: evt.target.name.value,
+      hp: evt.target.hp.value,
+      front: evt.target.frontUrl.value,
+      back: evt.target.backUrl.value
+    }
+    
+    fetch(`http://localhost:3000/pokemon`, {
+      method:'POST',
+     headers: { 
+         'Content-type': 'application/json',
+         'accept': 'application/json'
+     },
+     body: JSON.stringify({
+      name: pokeinfo.name,
+      stats: [{
+        name: "hp",
+        value: pokeinfo.hp
+      }],
+      sprites: {
+        front: pokeinfo.front,
+        back: pokeinfo.back
+      } 
+      })
+    })
+    .then(resp => resp.json())
+    .then(addedPokeObj => this.setState((prevState) => {
+      return {
+        pokeArray: [...prevState.pokeArray, addedPokeObj ]
+      }
+    }
+    ))
+  }
+
+  whichArray = () => {
+    let newArray = [...this.state.pokeArray]
+    if (this.state.searchTerm === ""){
+      newArray = [...this.state.pokeArray]
+    } else {
+      newArray = newArray.filter(pokemon => pokemon.name.includes(this.state.searchTerm))
+    }
+    return newArray
+  }
+
+  componentDidMount(){
+    fetch(`http://localhost:3000/pokemon`)
+    .then(resp => resp.json())
+    .then(res => this.setState({pokeArray: res}))
+  }
+
+
   render() {
+
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm handleSubmit={this.handleSubmit}/>
         <br />
-        <Search onChange={() => console.log('🤔')} />
+        <Search onChange={this.handleSearch} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokeArray={this.whichArray()} />
       </Container>
     )
   }
